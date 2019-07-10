e7ef1c7bfede50495a531c7469d698355ec1f801
Doug Kintop <dkintop@hotmail.com>
2019-07-10 17:39:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -75,9 +75,16 @@ total = 0
 clearance_applied.each do |item, value|
   total_price = value[:price] * value[:count]
 total = total_price + total
-
+ end
+if total > 100 
+  discount = total * 0.10
+  final_total = total - discount
+  
+else
+final_total = total
 end
-binding.pry
+final_total
+
 end
 
 
